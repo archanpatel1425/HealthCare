8c54af62f2a2b4b21f36e13c8ed29781d5abc621
Archan Patel <archanpatel6070@gmail.com>
2025-01-26 19:09:41 +0530

    added 3 pages and backend initial setup was done

--- client/src/components/HomPage/HeroSection.jsx
+++ client/src/components/HomPage/HeroSection.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import HeroSectionImage from '../../assets/HeroSectionImage.jpg';
+import HeroSectionImage1 from '../../assets/img2.png';
 const HeroSection = () => {
     return (
         <div className=''>
             <div className="h-screen pt-16">
-                <div className="bg-[#ebf8ef] w-full h-full ">
+                <div className="bg-[#d4e8db] w-full h-full ">
                     <div className='flex justify-between items-center mx-24'>
                         <div>
                             <div className=' text-5xl font-bold'>
@@ -12,13 +12,16 @@ const HeroSection = () => {
                                 Needs Is Our Priority
                             </div>
                             <div></div>
-                            <button className='text-white bg-green-600 p-4 rounded-full mt-8 font-bold'>book An Appointment</button>
+                            <button
+                                className='text-white bg-green-600 p-4 rounded-full mt-8 font-bold'
+                            >
+                                book An Appointment
+                            </button>
                         </div>
-                        <img src={HeroSectionImage} alt="" className='h-[70vh] w-[35vw]' />
+                        <img src={HeroSectionImage1} alt="" className='mt-8 h-[85vh] w-[40vw]' />
                     </div>
                 </div>
             </div>
-
         </div>
     );
 };
