a74ea0961e5e47ecaa9892205e6d74ab7907a98b
jay-badgujar <jaybadgujar18@gmail.com>
2025-01-26 19:14:45 +0530

    testing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,26 @@
-import Navbar from './components/Navbar'
-import Home from './pages/doctor-panel/Home'
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import DoctorSignUp_Form from './pages/DoctorSignUp_Form';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import PatientSignUp_Form from './pages/PatientSignUp_Form';
 
 function App() {
+  const location = useLocation();
+
+  const pathsWithNavbar = ['/', '/about', '/contactus'];
 
   return (
     <div>
-      {/* <Navbar /> */}
-      <Home />
-      {/* <Footer  /> */}
+      {pathsWithNavbar.includes(location.pathname) && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup/doctor" element={<DoctorSignUp_Form />} />
+        <Route path="/signup/patient" element={<PatientSignUp_Form />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
