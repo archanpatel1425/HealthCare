05ddddcbff025c3ff540c5eadc258f7ce70707ca
1JAYPANDYA1 <jaypandya1911@gmail.com>
2025-02-03 23:00:02 +0530

    latest code

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,13 @@
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import express from 'express'
-
 import { MY_IP } from './config/envConfig.js'
 import authRoutes from './routes/authRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
-
 const app = express();
 const allowedOrigins = [
     `http://${MY_IP}:5173`,
-    'http://localhost:5173',
-];
-
+    'http://localhost:5173',];;
 const corsOptions = {
     origin: allowedOrigins,
     methods: "GET,POST,PUT,DELETE",
--- server/src/utils/tokenUtils.js
+++ server/src/utils/tokenUtils.js
@@ -2,8 +2,8 @@ import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
 
 dotenv.config()
-export const generateAccessToken = (userId) => {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '50m' });
+export const generateAccessToken = (userId, userType) => {
+  return jwt.sign({ id: userId, usertype: userType }, process.env.JWT_SECRET, { expiresIn: '50m' });
 };
 
 export const generateRefreshToken = (userId) => {
@@ -19,11 +19,11 @@ export const verifyAccessToken = (req, res, next) => {
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.userId = decoded.id;
-    // return decoded.id
+    req.usertype = decoded.usertype
     next();
   } catch (error) {
     console.log(error)
-    res.status(401).json({ success: false, message: 'Unauthorized: Invalid token' });
+    res.status(401).json({ success: false, message: 'Unauthorized: Inva lid token' });
   }
 };
 
