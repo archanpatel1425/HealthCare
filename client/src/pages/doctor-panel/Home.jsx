9282f4eeb9a9c0fba8a5d36150bfd02c7e1296e4
Archan <archanpatel6070@gmail.com>
2025-02-11 21:05:04 +0530

    changes of docter panel of layout

--- client/src/pages/doctor-panel/Home.jsx
+++ client/src/pages/doctor-panel/Home.jsx
@@ -1,42 +1,27 @@
-import React, { useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import Sidebar from "./Sidebar";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import DoctorLayout from "../../components/Docter/DoctorLayout";
+import ConsultedPatients from "./ConsultedPatients";
 import Dashboard from "./Dashboard";
 import NewAppointments from "./NewAppointments";
-import UpComingAppointments from "./UpComingAppointments";
-import ConsultedPatients from "./ConsultedPatients";
-import Alerts from "./Alerts";
-import Profile from "./Profile";
 import PrescriptionForm from "./PrescriptionForm";
-import Navbar from './Navbar'
+import Profile from "./Profile";
+import UpComingAppointments from "./UpComingAppointments";
 
 export default function DoctorPanel() {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-
-  const toggleSidebar = () => setSidebarOpen(!sidebarOpen);
 
   return (
-    <div className="flex h-screen bg-gray-100">
-      {/* Sidebar */}
-      <Sidebar sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
-      {/* Main Content */}
-      <div className="flex-1 flex flex-col">
-        {/* Navbar */}
-        <Navbar sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
-        {/* <Navbar /> */}
-        <Alerts sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar}/>
-        {/* Content */}
-        <div className="p-6 bg-gray-50 flex-1">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/new-appointments" element={<NewAppointments />} />
-            <Route path="/upcoming-appointments" element={<UpComingAppointments />} />
-            <Route path="/consulted-patients" element={<ConsultedPatients />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/prescription-form" element={<PrescriptionForm />} />
-          </Routes>
-        </div>
-      </div>
-    </div>
+    <div className="flex h-screen bg-gray-100 ">
+      <DoctorLayout >
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/new-appointments" element={<NewAppointments />} />
+          <Route path="/upcoming-appointments" element={<UpComingAppointments />} />
+          <Route path="/consulted-patients" element={<ConsultedPatients />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/prescription-form" element={<PrescriptionForm />} />
+        </Routes>
+      </DoctorLayout>
+    </div >
   );
 }
